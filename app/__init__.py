4fa1b104efd7dc918c95937f38bbeb7313de8504
yunnppa <yunnpopoaung2018@gmail.com>
2025-07-05 05:53:19 +0630

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-def create_app(): #applied SRP
-    app = Flask(__name__)
-
-    from app.views import main
-    app.register_blueprint(main)
-
-    return app
+    def create_app():  #applied SRP
+        app = Flask(__name__)
+    
+        from app.views import main
+        app.register_blueprint(main)
+    
+        return app
